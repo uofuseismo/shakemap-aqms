24711fa8c995e7e9e93edfcf85ae047de09fde43
ggann <48806361+ggann@users.noreply.github.com>
2020-05-19 15:02:38 -0700

    Update aqms_queue.sh
    Adding extra logic to help clear out pid lock files upon unexpected exits

--- scripts/aqms_queue.sh
+++ scripts/aqms_queue.sh
@@ -3,15 +3,26 @@
 start() {
   source /home/shake/.bashrc
   conda activate shakemap
+  rm -f /home/shake/shakemap_profiles/default/install/logs/aqms_queue.pid.lock
+  rm -f /home/shake/shakemap_profiles/default/install/logs/aqms_queue.pid
   python /home/shake/shakemap-aqms/bin/aqms_queue &
   echo $! > /tmp/aqms_queue.pid
+#  fi
 }
 
 stop() {
   kill $(cat /tmp/aqms_queue.pid)
-  rm /home/shake/shakemap_profiles/default/install/logs/aqms_queue.pid
+  rm -f /home/shake/shakemap_profiles/default/install/logs/aqms_queue.pid
 }
+
 
 case $1 in
   start|stop) "$1";;
+  restart|reload|condrestart)
+        stop
+        start
+        ;;
+  *)
 esac
+
+exit 0
